5d7cdddea8697c4b07c4c920a514e8e1e993f554
iceqiw <qqwei1123@163.com>
2017-09-14 16:41:19 +0800

    add data

--- src/Component/index.jsx
+++ src/Component/index.jsx
@@ -4,20 +4,80 @@ import { is, fromJS } from 'immutable';
 import { Tool } from '../Config/Tool';
 import { Header, template } from './common/mixin';
 
+
+class Table extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return !is(fromJS(this.props), fromJS(nextProps)) || !is(fromJS(this.state), fromJS(nextState))
+    }
+
+    render() {
+        return (
+            <table className="table ">
+                <thead>
+                    <tr>
+                        <th >车次</th>
+                        <th >日期</th>
+                        <th >始发站</th>
+                        <th >终点站 </th>
+                        <th >硬卧</th>
+                        <th >硬座</th>
+                        <th >软卧</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        this.props.list.map((item, index) => {
+                            return <TableRow key={index} {...item} index={index} />
+                        })
+                    }
+                </tbody>
+            </table>
+        );
+    }
+}
+
+class TableRow extends Component {
+    constructor(props, context) {
+        super(props, context)
+        this.state = {
+
+        }
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return !is(fromJS(this.props), fromJS(nextProps)) || !is(fromJS(this.state), fromJS(nextState))
+    }
+
+    render() {
+        let { num_yw, num_yz, num_rw, train,end_station,start_station,date} = this.props;
+        return (
+            <tr>
+                <td >{train}</td>
+                <td >{date}</td>
+                <td >{start_station}</td>
+                <td >{end_station}</td>
+                <td >{num_yw}</td>
+                <td >{num_yz}</td>
+                <td >{num_rw}</td>
+            </tr>
+        );
+    }
+}
+
 class Main extends Component {
     constructor() {
         super();
         this.state = {
-            topic: '[1]'
+            productList: [],
         }
     }
 
 
     componentWillMount() {
         console.log("componentWillMount")
-        this.props.getData('/api/train/search/2017-10-01/1/1/1', {}, (resp) => {
+        this.props.getData('/api/train/search/2017-10-01/FZS/WAR/T306', {}, (resp) => {
             console.log(resp)
-            this.setState(resp.data)
+            this.setState({productList:resp.data})
         }, 'input')
 
     }
@@ -39,24 +99,7 @@ class Main extends Component {
             <div>
                 <Header title='index' />
                 <div className="container">
-                    <table className="table ">
-                        <thead>
-                            <tr>
-                                <th >硬卧</th>
-                                <th >硬座</th>
-                                <th >软卧</th>
-                                <th >车次</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td >{this.state.num_yw}</td>
-                                <td >{this.state.num_yz}</td>
-                                <td >{this.state.num_rw}</td>
-                                <td >{this.state.train}</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <Table list={this.state.productList} />
                 </div>
             </div>
         );
